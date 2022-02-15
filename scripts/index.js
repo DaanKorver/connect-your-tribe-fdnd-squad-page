7a26798d65b1c783036fe8e10ab068a0abfa62c2
DaanKorver <daank2000@hotmail.nl>
2022-02-15 15:13:56 +0100

    finished playlist

--- scripts/index.js
+++ scripts/index.js
@@ -11,15 +11,44 @@ async function getMembers() {
 
 async function fillPlaylist() {
   const members = await getMembers()
-  members.forEach((index, member)=>{
+  members.forEach(async (member, index)=>{
     const song = songEl.content.cloneNode(true)
     const td = song.querySelectorAll('td')
-    const memberData = ['name', 'squadId', 'githubHandle', 'url'].map(key => member[key])
+    const fullName = `${member.name} ${member.prefix} ${member.surname}`
+    const githubHandle = getGithubHandle(member.githubHandle)
+    const squad = await getSquad(member.squadId)
+    const memberData = [fullName, squad, githubHandle, member.url, member.avatar]
     for (let i = 0; i < td.length; i++) {
-      if(i === 0) td[i].innerText = index
-      else td[i].innerText = memberData[i]
-      
+      if(i === 0) {
+        td[i].innerText = index+1
+      } else if(td[i].classList.contains('name')) {
+        const avatar = memberData[memberData.length-1] || './assets/empty.svg'
+        const className = memberData[memberData.length-1] ? '' : 'empty'
+        const image = td[i].querySelector('img')
+        const span = td[i].querySelector('span')
+        span.innerText = memberData[i - 1]
+        image.src = avatar
+        if(className) image.classList.add(className)
+      } else td[i].innerText = memberData[i - 1]
     }
+    playlistEl.addEventListener('click', ()=>{
+      navigate(member.memberId)
+    })
     playlistEl.appendChild(song)
   })
+}
+
+function navigate(memberId) {
+  window.location = `detail.html?id=${memberId}`
+}
+
+async function getSquad(squadId) {
+  const req = await fetch('https://tribe.api.fdnd.nl/v1/squad')
+  const res = await req.json()
+  return res.data.find(squad => squad.squadId == squadId).name
+}
+
+function getGithubHandle(handle) {
+  if(handle === '') handle = 'FDND'
+  return `@${handle.replace(/https:\/\/github\.com\//gm, '')}`
 }
