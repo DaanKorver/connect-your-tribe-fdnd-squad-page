2bac038f505312e9c8459fcc54dfca91e51045c7
boudewijnbout <boudewijn_bout@outlook.com>
2022-02-28 12:47:33 +0100

    Added personal information

--- scripts/details.js
+++ scripts/details.js
@@ -5,9 +5,13 @@ const bannerImage = document.querySelector('.banner-content > img ');
 
 const titleName = document.querySelector('.banner h1');
 const personDescription = document.querySelector('.banner p');
-const personalImages = document.querySelectorAll('.personal-info img');
 
-render()
+const personalImages = document.querySelectorAll('.personal-info img');
+const personalInfoName = document.querySelector('#personal-info-name');
+const personalInfoSocial = document.querySelector('#personal-info-social');
+const personalInfoUrl = document.querySelector('#personal-info-url');
+
+render();
 
 // Get data according to ID
 async function getMemberById() {
@@ -20,12 +24,17 @@ async function getMemberById() {
 async function render() {
     const member = await getMemberById()
     const avatar = member.avatar !== '' ? member.avatar : 'https://source.unsplash.com/random/1920x1080';
-    titleName.innerText = `${member.name} ${member.prefix}${member.surname}`
+
+    titleName.innerText = `${member.name} ${member.prefix}${member.surname}`;
     personDescription.innerText = member.bio;
     
     personalImages.forEach(popularImage => {
         popularImage.src = avatar;
     });
+
+    personalInfoName.innerText = `${member.name} ${member.surname}`;
+    personalInfoSocial.innerText = `@${member.githubHandle}`;
+    personalInfoUrl.innerText = `${member.url}`;
 
     bannerImage.src = avatar;
 }
